3bb49071226084bbdb51e4b855672d95d6e11feb
Helmsman <111644516+UberHelmsman@users.noreply.github.com>
2025-03-22 16:03:46 +0200

    add: task3 finished

---
+++ web_lab_3/task3.php
@@ -0,0 +1,11 @@
+<?php
+
+$numLanguages = 4;
+
+$months = 11;
+
+$days = $months * 16;
+
+$daysPerLanguage = $days / $numLanguages;
+
+echo "колво дней на язык программирования (наверное без сна): ", $daysPerLanguage;
