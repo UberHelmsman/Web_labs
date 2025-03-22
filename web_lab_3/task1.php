bb95a509034659bbde2afb5c305cf7e5cc68185d
Helmsman <111644516+UberHelmsman@users.noreply.github.com>
2025-03-22 15:46:29 +0200

    последствия тирании комфорта современных языков

--- web_lab_3/task1.php
+++ web_lab_3/task1.php
@@ -4,7 +4,7 @@ $very_bad_unclear_name = "15 chicken wings";
 
 // Write your code here:
 $order = $very_bad_unclear_name;
-$order .= 'and a diet coke'
+$order .= 'and a diet coke';
 
- // Don't change the line below
+// Don't change the line below
 echo "\nYour order is: $very_bad_unclear_name.\n";
