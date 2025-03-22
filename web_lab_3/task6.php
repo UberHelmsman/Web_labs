8671004d3cbf7e1dab9948a68784fd7a7846cd74
Helmsman <111644516+UberHelmsman@users.noreply.github.com>
2025-03-22 21:56:50 +0200

    add: task6 finished

--- web_lab_3/task6.php
+++ web_lab_3/task6.php
@@ -23,10 +23,55 @@ foreach ($m as $k => $v) {
 
 $square_root_2 = sqrt($sum);
 
-$num = 379;
-$numSqrt = sqrt($num);
+
+$numSqrt = sqrt(379);
 $numWhole = round($numSqrt, 0);
 $numDec = round($numSqrt, 1);
 $numHund = round($numSqrt, 2);
 
-echo "$numSqrt, $numWhole, $numDec, $numHund";
+echo "$numSqrt, $numWhole, $numDec, $numHund \n\n";
+
+$num2 = sqrt(587);
+$num2RoundedUp = ceil($num2);
+$num2RoundedDown = floor($num2);
+
+$m2 = [4,-2,5,19,-130,0,10];
+$m2Min = min($m2);
+$m2Max = max($m2);
+
+echo rand(0,100),"\n\n";
+
+$m3 = [];
+for ($i = 0; $i < 10; $i++) {
+    $r = rand(0,100);
+    array_push($m3,$r);
+    echo $m3[$i], " ";
+}
+
+
+$d = abs($a-$b);
+echo "\n$d \n";
+$a += 2;
+$b += 20;
+$d2 = abs($a-$b);
+echo $d2, "\n";
+
+$m4 = [1,2,-1,-2,3,-3];
+for ($i = 0; $i < count($m4); $i++) {
+    $m4[$i] = abs($m4[$i]);
+    echo $m4[$i]," ";
+}
+
+$num3 = 30;
+$dels = [];
+for ($i = 1; $i < $num3; $i++) {
+    if ($num3 % $i == 0) array_push($dels,$i);
+}
+
+$m5 = [1,2,3,4,5,6,7,8,9,10];
+for ($i = 0; $i < count($m5); $i++) {
+    if (array_sum(array_slice($m5,0,$i)) > 10 ) {
+        echo "\nНужно сложить первые $i элементов\n"; 
+        break;
+    }
+}
