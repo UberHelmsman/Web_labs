4c4c0ae3229abdf5dc3c2ea63e3cf8c8661ac96d
Helmsman <111644516+UberHelmsman@users.noreply.github.com>
2025-05-14 11:17:59 +0200

    add: requests.php basic request funcions

---
+++ web_lab_6/requests.php
@@ -0,0 +1,56 @@
+<?php
+// GET запросик 
+function EchoBasicGetRequest(){
+    $ch = curl_init('https://jsonplaceholder.typicode.com/posts/1'); // это там французский что ли или лорем ипсум какой то
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($ch);
+    curl_close($ch);
+    echo "GET: " . $response . "\n";
+}
+
+
+// POST запрос
+function EchoBasicPostRequest(){
+    $ch = curl_init('https://jsonplaceholder.typicode.com/posts');
+    $stuff = [
+        'title' => "dsdfghjdfggdsgdgfh",
+        'body' => "e5tydwftvadsfsadrbv",
+        'userId' => "weopkcvdcaq"
+    ];
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($stuff));
+    $response = curl_exec($ch);
+    curl_close($ch);
+    echo "POST: " . $response . "\n";
+}
+
+
+// PUT запрос
+function EchoBasicPutRequest(){
+    $ch = curl_init('https://jsonplaceholder.typicode.com/posts/1');
+    $stuff = [
+        'title' => "qwopiertop",
+        'body' => "qzcxopiwerkml"
+    ];
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+    curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($stuff));
+    $response = curl_exec($ch);
+    curl_close($ch);
+    echo "PUT: " . $response . "\n";
+}
+
+
+// DELETE запрос
+function EchoBasicDeleteRequest(){
+    $ch = curl_init('https://jsonplaceholder.typicode.com/posts/1');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
+    $response = curl_exec($ch);
+    curl_close($ch);
+    echo "DELETE: " . $response . "\n";
+}
+?>
